a6d1b03a3361a0517d0b73ccd73530200dc90016
Anderson Pereira da Silva <andersonpds17042000@gmail.com.com>
2025-04-10 23:15:53 -0400

    primeiro comite

---
+++ src/pages/CartPage/index.tsx
@@ -0,0 +1,45 @@
+import Typography from "../../components/Typography";
+import Styles from "./CartPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+import { Product } from "../../common/types/product";
+import CartList from "../../components/CartList";
+import CartSummary from "../../components/CartSummary";
+
+type CartPageProps = {
+  cartItems: Product[];
+  cartCount: number;
+  removeFromCart: (id: number) => void;
+};
+
+const CartPage = ({ cartItems, removeFromCart, cartCount }: CartPageProps) => {
+  const total = cartItems.reduce((acc, item) => acc + item.price, 0);
+  const freight = cartItems.length > 0 ? 8 : 0;
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => navigate("/");
+
+  const handlePayment = () => console.log("pagamento");
+
+  return (
+    <main className="container">
+      <div className={Styles.cartTitle}>
+        <Typography variant="h4">Carrinho de Compras</Typography>
+      </div>
+
+      <section className={Styles.cartPage}>
+        <CartList cartItems={cartItems} onRemove={removeFromCart} />
+        <CartSummary
+          itemCount={cartCount}
+          total={total}
+          freight={freight}
+          onPayment={handlePayment}
+          handleRedirect={handleRedirect}
+        />
+      </section>
+    </main>
+  );
+};
+
+export default CartPage;
---
+++ src/pages/ProductDetailsPage/index.tsx
@@ -0,0 +1,59 @@
+import { useParams } from "react-router-dom";
+import Styles from "./ProductDetailsPage.module.css";
+import Typography from "../../components/Typography";
+import ProductDetail from "../../components/ProductDetail";
+import { PRODUCTS_BASE_URL } from "../../common/constants/endpoints";
+import { Product } from "../../common/types/product";
+import StatusHandler from "../../common/utils/statusHandler";
+import BackgroundBanner from "../../components/BackgroundBanner";
+import useFetch from "../../common/hooks/useFetch";
+
+type ProductDetailsPageProps = {
+  addToCart: (product: Product) => void;
+};
+
+function ProductDetailsPage({ addToCart }: ProductDetailsPageProps) {
+  const { id } = useParams<{ id: string }>(); // Pega o ID da URL
+
+  const {
+    data: productsData,
+    isLoading,
+    error,
+  } = useFetch<{ products: Product[] }>(PRODUCTS_BASE_URL);
+
+  // Encontrar o produto com base no ID
+  const product = productsData?.products.find(
+    (product) => product.id.toString() === id
+  );
+
+  return (
+    <>
+      <BackgroundBanner backgroundImage="https://raw.githubusercontent.com/gss-patricia/use-dev-assets/refs/heads/main/banner-secoes.png" />
+      <main className="container">
+        <section>
+          <div className={Styles.productContainer}>
+            <Typography variant="h4">Detalhes do Produto</Typography>
+
+            <StatusHandler isLoading={isLoading} error={error}>
+              {product ? (
+                <ProductDetail
+                  id={product.id}
+                  title={product.label}
+                  description={product.description}
+                  price={product.price}
+                  imageUrl={product.imageSrc}
+                  colors={product.colors}
+                  addToCart={addToCart}
+                />
+              ) : (
+                <p>Produto não encontrado.</p>
+              )}
+            </StatusHandler>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default ProductDetailsPage;
